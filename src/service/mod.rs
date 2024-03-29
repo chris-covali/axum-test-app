0d4e3113508a51bcc8ddc7199e920ae0a6b2f3f8
Cristian Covali <kow.kristi@gmail.com>
2024-03-29 14:17:40 +0200

    feat: Initial commit

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -13,17 +13,3 @@ impl Services {
         }
     }
 }
-
-pub fn solve(s: String) -> Vec<String> {
-    let mut words: Vec<String> = s.split(" ").into_iter().map(|s| s.to_string()).collect::<Vec<_>>();
-    let len = words.len();
-
-    for i in 0..len {
-        let cur_wrd = words.get(i).unwrap();
-        let nxt_wrd = words.get(len % (i + i)).unwrap();
-        let prv_idx = if i == 0 { len - 1 } else { i - 1 };
-        let prv_wrd = words.get((len % (i + 1)) - 1).unwrap();
-    }
-
-    return result;
-}
